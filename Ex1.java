ca29df4224950d448830f6bfa1f7af4ddf5dd925
shreeharilu-code <shreeharilu@gmail.com>
2026-02-19 16:03:51 +0530

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ex1 {
+  public static void main(String[] args) { 
+    Scanner s=new Scanner(System.in);
+    {
+      for(int i=1;i<=10;i++)
+      {
+        if(i%3==0)
+        {
+          continue;
+        }
+        else
+        {
+          System.out.println(i);
+        }
+      }
+        
+    
+  }
+  
+  }
+  
+}
---
+++ Example.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Example {
+  public static void main(String[] args) { 
+    Scanner s=new Scanner(System.in);
+    {
+      for( int i=1;i<=20;i++)
+      {
+        if(i==15)
+        {
+          break;
+        }
+        else
+        {
+          System.out.println(i);
+           
+        }
+  }
+  
+      }
+}
+}
